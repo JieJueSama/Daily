48d60dbf2cb7eba5503cc71e662d2e633e00feab
zhaojiaxin <446212027@qq.com>
2020-01-03 18:32:47 +0800

    多线程

--- JavaSE/workspace_idea/JavaSenior/src/com/zjx/java/ThreadTest.java
+++ JavaSE/workspace_idea/JavaSenior/src/com/zjx/java/ThreadTest.java
@@ -17,7 +17,7 @@ class MyThread extends Thread{
     public void run() {
         for (int i = 0; i < 100; i++) {
             if(i % 2 == 0){
-                System.out.println(i);
+                System.out.println(Thread.currentThread().getName() + ":" + i);
             }
         }
 
@@ -32,12 +32,23 @@ public class ThreadTest {
 //        3.创建Thread类的子类的对象
         MyThread myThread = new MyThread();
 
-//        4.通过此对象调用start()
+//        4.通过此对象调用start()：启动当前线程  调用当前线程的run()
         myThread.start();
 
+        //问题一：我们不能通过直接调用run()的方式启动线程
+//        myThread.run();
+
+        //问题二：再启动一个线程，遍历100以内的偶数。不可以还让已经start()的线程去执行，会报 IllegalThreadStateException
+//        myThread.start();
+        //我们需要重新创建一个线程的对象
+        MyThread myThread1 = new MyThread();
+        myThread1.start();
+
+
+        //如下操作仍然是在main线程中执行的
         for (int i = 0; i < 100; i++) {
             if(i % 2 == 0){
-                System.out.println(i + "*****************main()***************");
+                System.out.println(Thread.currentThread().getName() + ":" + i + "*****************main()***************");
             }
         }
     }
